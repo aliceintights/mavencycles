edabc417dc0216f490975605f573ebcb902deac6
irina <obesnyuk@gmail.com>
2023-09-12 22:19:30 +0300

    csv added

--- src/test/java/YearlyIncomeTest.java
+++ src/test/java/YearlyIncomeTest.java
@@ -1,14 +1,17 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.services.YearlyIncome;
 
 public class YearlyIncomeTest {
 
-    @Test
-    public void shouldCalcYearlyIncome() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/vacation.csv")
+    public void shouldCalcYearlyIncome(int expected, int income, int expences, int threshold) {
         YearlyIncome service = new YearlyIncome();
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
+        int actual = service.calculate(income, expences, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
